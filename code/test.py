669d76753c6e441e87a695e04f634f0257f893fd
Daniele Noto <daniele_noto@yahoo.it>
2023-03-21 10:59:03 +0100

    added generate and expoenents, still need to sasave stat in a good way but produces resultats for MLP f=1

--- code/test.py
+++ code/test.py
@@ -1,29 +1,111 @@
+import argparse
 from shellmodels_lib import *
 from libraries import *
 
+parser = argparse.ArgumentParser(description='Train and generate trajectories using a shell model.')
+parser.add_argument('--model', type=str, default='MLP', choices=['MLP', 'LSTM'], help='Type of model to use')
+parser.add_argument('--h', type=int, default=1, help='Number of historical timesteps to consider')
+parser.add_argument('--f', type=int, default=25, help='Number of future timesteps to predict')
+parser.add_argument('--training', type=str, default='STD', choices=['STD', 'FOR'], help='Type of training to use')
+parser.add_argument('--optim', type=str, default='adam',choices=['adam','lbfgs'], help='Choose the optimizer')
+parser.add_argument('--lr', type=float, default=0.001, help='Learning rate for the optimizer')
+parser.add_argument('--epochs', type=int, default=10, help='Number of epochs for training')
+parser.add_argument('--batch-size', type=int, default=100, help='Batch size for training')
+parser.add_argument('--print-every', type=int, default=5, help='Print progress every N epochs')
+parser.add_argument('--nshells', type=int, default=12, help='Number of shells')
+parser.add_argument('--sampling', type=int, default=30, help='Sampling rate for the data')
+parser.add_argument('--run', type=int, default=1, help='Run number')
 
 
-h = 1
-f = 1
-model=MLP(100,12,h,f,0)
-#model=Sequence(100,1,12)
-optimizer=th.optim.Adam(model.parameters(),lr=0.001)
-data=load_data('../dataset/N12/','Uf_N12.npy',sampling=20)
-train_data,valid_data,test_data=split_data(data)
+args = parser.parse_args()
+run=args.run
+model_type = args.model
+h = args.h
+f = args.f
+training_type = args.training
+if training_type == 'STD':
+    f=1
+lr = args.lr
+optimizer_type=args.optim
+nepochs = args.epochs
+bsize = args.batch_size
+nprints = args.print_every
+nshells=args.nshells
+datapath ='../dataset/N'+str(nshells)+'/'
+if nshells==12:
+    datafile = 'Uf_N12.npy'
+elif nshells==19:
+    datafile = 'Uf_N19_1_.npy'
+sampling = args.sampling
+#----------------------param physical data----------------------#
+N=nshells
+knn=np.power(2,np.arange(N+4))
+kn=knn*np.power(2,-4.)
 
 
-#standard=STDtraining(mlp,optimizer,train_set,valid)
+def folder_generator(save_data=False,save_plot=False,save_model=False,
+                     training_type=training_type,model_type=model_type,nshells=nshells):
+
+    folder='../results/'+str(training_type)+'/'+str(model_type)+'/'+'N'+str(nshells)+'/'
+    
+    if save_model:
+        return folder+'model/'
+    if save_data:
+        return folder+'raw/'
+    if save_plot:
+        return folder+'plot/'
+print("folder",folder_generator(save_plot=True))
 
 
 
+if model_type == 'MLP':
+    model = MLP(100, nshells, h, f, 0)
+elif model_type == 'LSTM':
+    model = Sequence(100, 1, nshells)
+
+if optimizer_type=='adam':
+    optimizer = th.optim.Adam(model.parameters(), lr=lr)
+elif optimizer_type=='lbfgs':
+    optimizer = th.optim.LBFGS(model.parameters(),lr=lr)
+
+data, (max, min) = load_data(datapath, datafile, sampling=sampling)
+train_data, valid_data, test_data = split_data(data)
+
+if training_type == 'STD':
+        trainer = STDtraining(model, optimizer, train_data, valid_data,
+                          history=h, future=f, nepochs=nepochs, bsize=bsize, nprints=nprints, save=False)
+elif training_type == 'FOR':
+    trainer = FORtraining(model, optimizer, train_data, valid_data,
+                          history=h, future=f, nepochs=nepochs, bsize=bsize, nprints=nprints, save=False)
+
+train_loss, valid_loss = trainer.train_model()
 
 
-trainer = STDtraining(model, optimizer, train_data, valid_data, history=h, future=f, nepochs=100, bsize=32, nprints=2, save=False)
-t, v = trainer.train_model()
+traj = model.generate(train_data, len(data)).detach().numpy()
+regen=traj*(max-min)+min
+model_exps=np.zeros([5,nshells])
+true_exps=np.zeros([5,nshells])
 
+data_true=data*(max-min)+min
+for i in range(0,5): #evaluate struc funct from S_2 to S_10
+    s=struct_func(regen,(i+1)*2,h=h,f=f,title='empirical'+'run_'+str(args.run),save=False)
+    model_exps[i]=s[:,0]
 
-#print(get_param(standard,optimizer))
-#train,val=standard.train(mlp,optimizer,train_set,valid,10,10,save=False)
-#print(standard)
-#orecast=FORtraining()
-#print(forecast)
+    truth=struct_func(data_true,(i+1)*2,h=h,f=f,title='real'+'run_'+str(args.run),save=False)
+    true_exps[i]=truth[:,0]
+
+    #print(i)
+    #print(s.shape)
+
+model_exps_list=[]
+true_exps_list=[]
+
+for i in model_exps:
+    #np.save(folder_generator(),-exponents(i,kn=kn,nshells=nshells))
+    model_exps_list.append(-exponents(i,kn=kn,nshells=nshells).slope)
+    #print(model_exps)
+for j in true_exps:
+    true_exps_list.append(-exponents(j,kn=kn,nshells=nshells).slope)
+
+print("model exponents:",model_exps_list,"run number",str(run))
+print("true exponents:",true_exps_list,"run number",str(run))
