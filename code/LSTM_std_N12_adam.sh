1fc443adecabbb7ec6018c3c7c0277388b8f0439
Daniele Noto <daniele_noto@yahoo.it>
2023-04-26 18:22:20 +0200

    added new script bash to redo LSTM std 12

---
+++ code/LSTM_std_N12_adam.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=LSTM12std   # Job name
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=daniele_noto@yahoo.it     # Where to send mail
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --gres=gpu:1 			#Run on GPU
+#SBATCH --gres-flags=enforce-binding
+#SBATCH --cpus-per-task=10 
+#SBATCH --mem=16gb                     # Job memory request
+#SBATCH --time=24:00:00               # Time limit hrs:min:sec
+#SBATCH --output=./log/LSTM_std_N12_1_50_1000ep_%j.log   # Standard output and error log
+pwd; hostname; date
+
+for n in {1..50}
+	do
+	srun python test.py --run $n --epochs 1000 --training STD --nshells 12 --model LSTM --optim adam
+	echo "finished run " $n
+	done
+date
